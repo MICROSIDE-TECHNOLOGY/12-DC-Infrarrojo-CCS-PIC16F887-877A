ea76f9983404906c63a4db36426d1286432f33ec
vhperaltaj <victor.peralta@microside.com>
2023-07-12 17:20:42 -0600

    Initial commit

---
+++ Proyecto/PIC16F877A/Motor DC + Infrarrojo.c
@@ -0,0 +1,51 @@
+/*
+AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
+FECHA: JUNIO 2019
+*/
+
+/*
+---------------------------------------------------------------------------
+Esta práctica consiste en variar la velocidad de un motor por medio de un sensor
+SHARP usando los módulos PWM y ADC.
+---------------------------------------------------------------------------
+*/
+
+
+#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
+#device ADC=8                                   // Configura el ADC a 8 bits
+#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+
+int VALOR_ADC;                                  //Variable para almacenar el valor leído del ADC
+
+void SisInit()
+
+{
+    
+    setup_timer_2 (T2_DIV_BY_16, 254, 16);          //Configura Timer2
+
+    setup_ccp1 (CCP_PWM);    //Configura el módulo CCP para uso del PWM
+
+    setup_adc_ports (AN0);                                      //Configura solo el puerto A0 como entrada analógica
+
+    setup_adc (ADC_CLOCK_DIV_8);                                //Configura oscilador para la conversión
+
+}
+
+void main(void)
+
+{
+     SisInit();
+
+     while (1)
+
+          {
+
+               set_adc_channel (0);                             //Selecciona el canal 0 para la conversión
+
+               VALOR_ADC = read_adc ()*1.53;                    //Lee el valor del ADC y lo guarda en la variable
+
+               set_pwm1_duty (VALOR_ADC);                       
+
+          }
+
+}
